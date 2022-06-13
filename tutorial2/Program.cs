3e39230c1ea5ace55cb5d8bc8a788d76b338abef
magie <magie@LAPTOP-QOJU1GKT>
2022-06-13 13:02:38 +0200

    Dodaj pliki projektów.

---
+++ tutorial2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("elo");
+
+            Console.ReadLine();
+        }
+    }
+}
