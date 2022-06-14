17e63f2e78303d96d9454c6d416bc66428e80f3a
Piotr Magiera <magierapiotr162@gmail.com>
2022-06-14 15:57:03 +0200

    menu

---
+++ tutorial2/TDrawer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial2
+{
+    internal class TDrawer
+    {
+
+        public static void MenuDraw(int lvl)
+        {
+            Console.Clear();
+            if (lvl == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("easy");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else Console.WriteLine("easy");
+
+            if (lvl == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Medium");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else Console.WriteLine("Medium");
+
+            if (lvl == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Hard");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else Console.WriteLine("Hard");
+        }
+
+    }
+}
---
+++ tutorial2/TMainMenu.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace tutorial2
+{
+    internal class TMainMenu
+    {
+        public static bool lvlSelected = false;
+        public TMainMenu()
+        {
+            
+        }
+
+        public  void Selecting()
+        {
+
+            int lvl = 0;
+            TDrawer.MenuDraw(lvl);
+            while (!lvlSelected)
+            {
+                if (Console.KeyAvailable) 
+                {
+                    var key = Console.ReadKey();
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            if (lvl != 0)
+                            {
+                                lvl--;
+                                TDrawer.MenuDraw(lvl);
+                                break;
+                            }
+                            break;
+                        case ConsoleKey.DownArrow:
+                            if (lvl != 2)
+                            {
+                                lvl++;
+                                TDrawer.MenuDraw(lvl);
+                                break;
+                            }
+                            break;
+                        case ConsoleKey.Enter:
+                            lvlSelected = true;
+                            break;
+                    }
+                }
+
+            }
+        }
+
+/*        public void MenuDraw(int lvl)
+        {
+            Console.Clear();
+            if (lvl == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("easy");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else Console.WriteLine("easy");
+
+            if (lvl == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Medium");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else Console.WriteLine("Medium");
+
+            if (lvl == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Hard");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else Console.WriteLine("Hard");
+        }*/
+
+
+    }
+}
